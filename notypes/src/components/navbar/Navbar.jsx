61e5d3d05035968ca1007eaf7da6b0d1511e060f
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2022-04-10 01:02:19 +0300

    added search and header for navbar

--- notypes/src/components/navbar/Navbar.jsx
+++ notypes/src/components/navbar/Navbar.jsx
@@ -4,11 +4,10 @@ import {Routes, Route, BrowserRouter as Router,Link} from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuIcon from '@mui/icons-material/Menu';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-
+import ClearIcon from '@mui/icons-material/Clear';
 import logo from '../../images/Logo.png'
-import { Menu } from '@mui/material';
-
 
 
 
@@ -20,8 +19,8 @@ const Navbar = () => {
   const [menuBars, setMenuBars]=useState(false);
   const box = useRef(null);
   useOutsideAlerter(box);
-
-
+  const [searchToggle, setSearchToggle]=useState("container2")
+  const [inputSearch,setInputSearch]=useState()
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
@@ -41,25 +40,26 @@ const Navbar = () => {
   }
   
 
-
+  
   return (
-    
-        
           <div className='app__header' ref={box}>
-            {/* <div className='app__header'>
-              <div className='app__header-logo-text'>MyMovieStand</div>
-              <div className='app__navbar-right_login' id={menuBars?"droplogin":""}>
+            <div className='app__header-header'>
+              <div className='app__header-logo'>
+                <Typography variant='h1' component='h1'> MyMovieStand</Typography>
+              </div>
+              <div className='app__header-login' >
                 <Link to='/login'>Login</Link>
                 <Link to='/signup'>Sign up</Link>
               </div>
-            
-            </div> */}
+            </div>
+
+          <div className='app__header-navbar'>
             <div className='app__navbar-left'>
                 <div className='app__navbar-hamburgermenu'>
                   <Button onClick={()=>{setMenuBars(!menuBars)}}><MenuIcon fontSize='large'/></Button>
                 </div>
                 <div className='app__navbar-left_logo' >
-                  <Link to='/' ><img src={logo} alt='logo' />{/*<i className='logoName'>MovieUniverse</i>*/}</Link>
+                  <Link to='/' ><img src={logo} alt='logo' /></Link>
                 </div>
                 <div className='app__navbar-left_nav' id={menuBars?"dropdown":""} >
                     <nav >
@@ -72,23 +72,32 @@ const Navbar = () => {
                     </nav>
                 </div>
 
-                
-
-               
-                {/* <div className='app__navbar-left_search_under'>
-                  <TextField label="Search" id="outlined-size-small"  size="small" 
-                  sx={{backgroundColor:'white' }}/>
-                </div> */}
             </div>
               
             <div className='app__navbar-right'>
-              
-              <div className='app__navbar-right_search'>
-                  <Button><SearchIcon fontSize='medium'/></Button>
+               {/* <div class="search">
+                <input type="text" class="search__input" aria-label="search" placeholder="enter your search"/>
+                  <Button className='search__submit' aria-label="submit search"><SearchIcon fontSize='medium'/></Button>
+              </div> */}
+              <div className={searchToggle}>
+                <div className='icon'>
                   
+                  <Button className='search' onClick={()=>searchToggle==='container2'?setSearchToggle('container2 active'):setSearchToggle('container2')} aria-label="submit search">
+                    <SearchIcon fontSize='medium'/>
+                  </Button>
+                </div>
+                <div className="input">
+                  <input type="text" aria-label='search' placeholder="Search a movie" value={inputSearch}/>
+                  <Button className='clear' onClick={() => setInputSearch(() => "")}><ClearIcon/></Button>
+                </div>
               </div>
+              {/* <div className='app__navbar-right_search'>
+                  <Button className='search' onClick={()=>searchToggle==='container2'?setSearchToggle('container2 active'):setSearchToggle('container2')} aria-label="submit search"><SearchIcon fontSize='medium'/></Button>   
+              </div> */}
             </div>
+
           </div>
+        </div>
             
         
 
