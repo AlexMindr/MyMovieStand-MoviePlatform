e09de4ce83ddf7a0258c03440fcc6a3b915f42a4
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-05-25 17:53:48 +0300

    rename old client

--- client/src/containers/error/Errorpage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Errorpage= () => {
-  return (
-    <div style={{display:'flex',padding:'5%',alignItems:'center',justifyContent:'center',fontSize:'3rem'}}>Error, page not found</div>
-  )
-}
-
-export default Errorpage
